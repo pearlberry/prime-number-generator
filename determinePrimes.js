c1cca7dbb7c9a8437c45c64596ed02f908d7a98d
lauren <laurenchristianson@live.com>
2021-05-20 16:41:54 -0500

    updates!
    added error handling
    added test cases

--- determinePrimes.js
+++ determinePrimes.js
@@ -11,26 +11,21 @@ function isPrime(num) {
 // Add primes to prime number array within the user-specified range
 function findPrimes(lowerBound, upperBound) {
     var primeArray = [];
-    if(lowerBound < 0 || upperBound < 0){ throw new Error('Please use positive numbers.'); }
-    else if(lowerBound < upperBound) {
+    if(lowerBound < 0 && upperBound < 0){ throw new Error('Please use positive numbers.'); }
+    if (isNaN(lowerBound) || isNaN(upperBound) ){ throw new Error('Please use numbers.'); }
+    else if (lowerBound === upperBound){ throw new Error('Please use a valid range.'); }
+    else {
+        if(lowerBound > upperBound) {
+            [lowerBound, upperBound] = [upperBound, lowerBound];
+        }
         for (var i = lowerBound; i < upperBound; i++) {
-            if ( isPrime(i) ) {
+            if ( isPrime(i) && i > 1) {
                 primeArray.push(i);
             }
         }
         console.log(`The prime numbers between ${lowerBound} and ${upperBound} are: ` + primeArray);
         return primeArray;
     }
-    else if(lowerBound > upperBound) {
-        for (var i = upperBound; i < lowerBound; i++) {
-            if (isPrime(i)) {
-                primeArray.push(i);
-            }
-        }
-        console.log(`The prime numbers between ${upperBound} and ${lowerBound} are: ` + primeArray);
-        return primeArray;
-    }
-    else { throw new Error('Please use a valid range.'); }
 }
 
 module.exports = {
--- primes.test.js
+++ primes.test.js
@@ -16,15 +16,15 @@ const { isPrime, findPrimes } = require('./determinePrimes');
 
   test('expected prime numbers are returned for user specified ranges', () => {
     const arr1 = [7901,7907,7919]
-    const arr2 = [0,1,2,3,5,7,11,13,17,19]
+    const arr2 = [2,3,5,7,11,13,17,19]
 
-    expect(findPrimes(7900, 7920)).toEqual(arr1);
+    expect(findPrimes(7900,7920)).toEqual(arr1);
     expect(findPrimes(0,20)).toEqual(arr2);
   });
 
   test('expected prime numbers are returned for user specified ranges when lower bound > upper bound', () => {
     const arr1 = [9851,9857,9859,9871,9883,9887,9901,9907,9923,9929,9931,9941,9949,9967,9973]
-    const arr2 = [1,2,3,5,7]
+    const arr2 = [2,3,5,7]
 
     expect(findPrimes(10000, 9850)).toEqual(arr1);
     expect(findPrimes(10,1)).toEqual(arr2);
@@ -40,4 +40,20 @@ const { isPrime, findPrimes } = require('./determinePrimes');
     expect(() => {
       findPrimes(20,20)
     }).toThrow(Error);
+  });
+
+  test('user cannot enter non-numerical values', () => {
+    expect(() => {
+      findPrimes('hi',20)
+    }).toThrow(Error);
+  });
+
+  test('negative numbers are not returned', () => {
+    const arr = [2,3,5,7]
+    expect(findPrimes(-10, 9)).toEqual(arr);
+  });
+
+  test('0 and 1 are not prime numbers', () => {
+    const arr = [2,3,5,7]
+    expect(findPrimes(0, 10)).toEqual(arr);
   });
